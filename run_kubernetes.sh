1e87e7907e38ba19b88a9c9378f4148213a1158f
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2020-04-20 12:43:17 +0200

    chore(master): Run Kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="pabloalbaladejo/project-ml-microservice-kubernetes"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run project-ml-microservice-kubernetes --image="$dockerpath" --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward -n default deployment/project-ml-microservice-kubernetes 8000:80
 
