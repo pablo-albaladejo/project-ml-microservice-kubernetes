7ced501e65436c06944a7e2022c839e04dc7f417
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2020-04-20 12:00:32 +0200

    chore(master): Docker push script

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="pabloalbaladejo/project-ml-microservice-kubernetes"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag project-ml-microservice-kubernetes $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath 
